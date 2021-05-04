c3ad5760c3d08f1f0d4aaf6fbf7d905021f43dbd
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-04 16:47:54 +0530

    Fetching images in result show screen

--- src/screens/ResultShowScreen.js
+++ src/screens/ResultShowScreen.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 function ResultShowScreen({ navigation }) {
+  const [result, setResult] = useState([]);
+  const getResult = async () => {
+    const response = awaityelp.get(`/${navigation.getParam("id")}`);
+    setResult(response.data.photos);
+  };
+  console.log(result);
+  useEffect(() => {
+    getResult();
+  }, []);
   return (
     <View>
-      <Text>Results show{navigation.getParam("id")}</Text>
+      <Text>Results show</Text>
     </View>
   );
 }
