caed6139a45de09ec2f20256163d4a6cb2a3bdc2
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-04 15:20:24 +0530

    Filtering the results

---
+++ src/Components/ResultsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+function ResultsList({ title, data }) {
+  return (
+    <View>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import useYelp from "../hooks/useYelp";
 import Searchbar from "../Components/SearchBar";
+import ResultsList from "../Components/ResultsList";
 function SearchScreen() {
   const [search, setSearch] = useState("");
+  const resultsByPrice = useMemo(() => {
+    return { cheap: [], average: [], expensive: [] };
+  }, []);
   const [{ results, error }, submitSearch] = useYelp();
-
+  const filterResultsByPrice = () => {
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].price == "$") {
+        resultsByPrice.cheap.push(results[i]);
+      }
+      if (results[i].price == "$$") {
+        resultsByPrice.average.push(results[i]);
+      }
+      if (results[i].price == "$$$" || results[i].price == "$$$$") {
+        resultsByPrice.expensive.push(results[i]);
+      }
+    }
+  };
+  useEffect(() => {
+    filterResultsByPrice();
+  }, [results, error]);
   return (
     <View style={styles.background}>
       <Searchbar
@@ -13,7 +32,15 @@ function SearchScreen() {
         searchChange={(newSearch) => setSearch(newSearch)}
         submitSearch={() => submitSearch(search)}
       />
-      {error ? <Text>{error}</Text> : <Text>{results.length}</Text>}
+      {error ? (
+        <Text>{error}</Text>
+      ) : (
+        <>
+          <ResultsList title="Cost Effective" data={resultsByPrice.cheap} />
+          <ResultsList title="Bit Pricy" data={resultsByPrice.average} />
+          <ResultsList title="Big Spender" data={resultsByPrice.expensive} />
+        </>
+      )}
     </View>
   );
 }
