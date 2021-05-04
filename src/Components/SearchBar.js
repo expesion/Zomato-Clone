a9cd7c99043f4c951f0efbd3e60f204ef99c3ae7
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-04 13:58:57 +0530

    Search bar UI

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+function SearchBar() {
+  return (
+    <View style={styles.background}>
+      <Feather name="search" style={styles.icon} />
+      <TextInput placeholder="Search" style={styles.input} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    marginHorizontal: 15,
+    marginTop: 10,
+    borderRadius: 5,
+    flexDirection: "row",
+  },
+  input: {
+    flex: 1,
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 35,
+    color: "black",
+    alignSelf: "center",
+    marginHorizontal: 10,
+  },
+});
+export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Searchbar from "../Components/SearchBar";
 function SearchScreen() {
   return (
-    <View>
+    <View style={styles.background}>
+      <Searchbar />
       <Text>Seach Screen</Text>
     </View>
   );
 }
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: "white",
+  },
+});
 export default SearchScreen;
