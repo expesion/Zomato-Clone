2d816bcc6312c7103fba111acb766c01ed2b79a6
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-04 13:32:33 +0530

    first commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+function SearchScreen() {
+  return (
+    <View>
+      <Text>Seach Screen</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({});
+export default SearchScreen;
