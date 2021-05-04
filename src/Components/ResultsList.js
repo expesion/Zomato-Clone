38b283323d84582797c7491384ae5f0273ff7748
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-04 15:34:55 +0530

    Working on resultslist

--- src/Components/ResultsList.js
+++ src/Components/ResultsList.js
@@ -1,9 +1,19 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import uuid from "react-native-uuid";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 function ResultsList({ title, data }) {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        data={data}
+        keyExtractor
+        keyExtractor={() => uuid.v4()}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
     </View>
   );
 }
