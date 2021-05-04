4fa12ad41b1f8287b0bf75f9464d7d9e170a6c62
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-04 15:50:00 +0530

    showing additional info

--- src/Components/ResultsDetail.js
+++ src/Components/ResultsDetail.js
@@ -2,10 +2,25 @@ import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
 function ResultsDetail({ result }) {
   return (
-    <View>
-      <Text>{result.name}</Text>
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text style={styles.name}>{result.name}</Text>
+      <Text style={styles.name}>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   );
 }
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { marginLeft: 15 },
+  image: {
+    borderRadius: 5,
+    height: 120,
+    width: 250,
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
 export default ResultsDetail;
